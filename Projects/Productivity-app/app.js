7a85866ebec62331d6315270c725eec4528b068a
SiPhoN99 <rarodz.04@gmail.com>
2026-01-02 09:41:45 +0100

    Added the delete Logic for the tasks

--- Projects/Productivity-app/app.js
+++ Projects/Productivity-app/app.js
@@ -2,15 +2,27 @@ const input = document.getElementById("taskInput");
 const button = document.getElementById("addTaskBtn");
 const list = document.getElementById("taskList");
 button.addEventListener("click", function () {
-  const taskText = input.value;
+  const taskText = input.value.trim();
 
   if (taskText === "") {
     return;
   }
 
-  const li = document.createElement("li");
-  li.textContent = taskText;
-
-  list.appendChild(li);
+  addTask(taskText);
   input.value = "";
 });
+
+function addTask(text){
+  const li = document.createElement("li");
+  li.textContent = text;
+//Delete logic for each button
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "Delete";
+
+  deleteBtn.addEventListener("click", function(){
+    li.remove();
+  })
+
+  li.appendChild(deleteBtn);
+  list.appendChild(li);
+}
